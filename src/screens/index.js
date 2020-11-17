2ab204049b5682bfd1512cbe884e67c672175187
Nugroho-00 <snugroho211@gmail.com>
2020-11-17 10:37:13 +0700

    add index.js to main js

---
+++ src/screens/index.js
@@ -0,0 +1,24 @@
+import Register from './Register';
+import Login from './Login';
+import Forgot from './Forget';
+import ResetPassword from './Reset';
+import Home from './Home';
+import Categori from './Category';
+import Product from './Product';
+import PageDetail from './PageDetail';
+import Cart from './Cart';
+import Checkout from './Checkout';
+import Success from './Success';
+import Profile from './Profile';
+import ShippingAddress from './ShippingAddress';
+import AddAddress from './AddAddress';
+import UpdateAddress from './UpdateAddress';
+import Settings from './Settings';
+import UpdatePassword from './UpdatePassword';
+import UpdateProfile from './UpdateUser';
+import Splash from './SplashScreen';
+
+export {
+    Login, Register, Home, Forgot, Categori, Product, ResetPassword,
+    PageDetail, Cart, Checkout, Success, Profile, ShippingAddress, AddAddress,
+    UpdateAddress, Settings, UpdatePassword, UpdateProfile, Splash  } 
